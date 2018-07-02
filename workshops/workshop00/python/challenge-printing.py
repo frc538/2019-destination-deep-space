07d20de82e378ba0fd788e7d9ab399c8ef070cdf
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2018-07-01 23:02:08 -0500

    Create challenge-printing.py

---
+++ workshops/workshop00/python/challenge-printing.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+'''
+  File Name: challenge-printing.py
+  Author: Lawrence (Drew) Whisenant
+  Date Created: 01 July 2018
+  Date Last Modified: 01 July 2018
+  Python Version: 3.7
+'''
+
+# Using what you've learned about printing in Python, print the following to the screen,
+#   meeting the listed requirements.
+
+# Goal:
+# My name is NAME. I was born on MM/DD/YYYY.
+# My address is 00000 STREET NAME, CITY, STATE ZIP.
+# My favorite color is COLOR.
+#   But I also like OTHER_COLOR.
+
+# Requirements
+#   You cannout use periods in the print (other than sep or end).
+#   MM/DD/YYYY should be in its own print, alone, with 3 things passed in.
+#   00000 STREET NAME, CITY, STATE should be in its own print, alone, with 3 things passed in.
+#   There are a total of four lines.
+#   The fourth line starts with a tab. 
