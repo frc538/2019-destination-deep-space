685fd40d36917f5476ed6e3b5000954cb4eabd68
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2018-07-03 12:12:41 -0500

    Update ChallengePrinting.java

--- workshops/workshop01/java/ChallengePrinting.java
+++ workshops/workshop01/java/ChallengePrinting.java
@@ -1,1 +1,35 @@
+/**
+ * File Name: ChallengePrinting.java
+ * Author: Lawrence (Drew) Whisenant
+ * Date Created: 03 July 2018
+ * Data Last Modified: 03 July 2018
+ * Java Version: 8
+ */
 
+public class ChallengePrinting {
+  public static void main(String[] args) {
+    /*    
+      Using what you've learned about printing in Java, 
+       print the following to the screen, 
+       meeting the listed requirements.
+    */
+    
+    /*
+      Goal:
+       My name is NAME. I was born on MM/DD/YYYY.
+       My address is 00000 STREET NAME, CITY, STATE ZIP.
+       My favorite color is COLOR.
+         But I also like OTHER_COLOR.
+    */
+
+    /* 
+      Requirements
+        You cannot use periods in the print (other than sep or end).
+        MM/DD/YYYY should be in its own print.
+        00000 STREET NAME, CITY, STATE should be in its own print.
+        There are a total of four lines.
+        The fourth line starts with a tab. 
+        You can only use one println.
+     */
+  }
+}
