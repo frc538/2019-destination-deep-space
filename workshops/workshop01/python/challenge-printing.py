a7187c315b3a55d8c480be8287a47aebad985d90
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2018-07-03 12:13:05 -0500

    Update challenge-printing.py

--- workshops/workshop01/python/challenge-printing.py
+++ workshops/workshop01/python/challenge-printing.py
@@ -3,7 +3,7 @@
   File Name: challenge-printing.py
   Author: Lawrence (Drew) Whisenant
   Date Created: 02 July 2018
-  Date Last Modified: 02 July 2018
+  Date Last Modified: 03 July 2018
   Python Version: 3.7
 '''
 
@@ -17,7 +17,7 @@
 #   But I also like OTHER_COLOR.
 
 # Requirements
-#   You cannout use periods in the print (other than sep or end).
+#   You cannot use periods in the print (other than sep or end).
 #   MM/DD/YYYY should be in its own print, alone, with 3 things passed in.
 #   00000 STREET NAME, CITY, STATE should be in its own print, alone, with 3 things passed in.
 #   There are a total of four lines.
