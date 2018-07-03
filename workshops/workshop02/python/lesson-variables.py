9ef7b849f175fd8179bc283b1f02fbb351193ec0
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2018-07-03 12:28:10 -0500

    Create lesson-variables.py

---
+++ workshops/workshop02/python/lesson-variables.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+'''
+  File Name: lesson-variables.py
+  Author: Lawrence (Drew) Whisenant
+  Date Created: 03 July 2018
+  Date Last Modified: 03 July 2018
+  Python Version: 3.7
+'''
+
+# Topics
+# 1. Variables
+# 2. Types and Casting
+# 3. Math and Logic
+
+# 1. Variables
+# Now that you can print to the screen, you'll be able to see what 
+#  the computer is doing with your program as it runs.
+
+# For print, you supplied some data into the function immediately.
+# But what if you want to put some data in your program to be used later?
+
+# This is where variables come in.
+# In math, a variable is an unknown or something whose value can change.
+# In programming, a variable is a piece of memory that can hold information.
+
+# Consider: x = 3
+# In math, we say, "The variable x is equal to the value 3," or "x is 3."
+# In programming, this sentence reads, "The variable x gets the value 3," or "3 is stored in x."
+
+# When programming in Python, = does not mean "equals," it means assignment.
+# For that reason, it is called the assignment operator.
+# Whenever you see it, think of it as "I am putting the right value into the memory on the left."
+
+# You can use variables with print.
+# You can uncomment both lines or just one, but notice what happens if the first line stays commented.
+# x = 3
+# print(x)
+
+# Generally, you want to name your variables something more meaningful than x.
+# Variables should start with a lowercase letter and have no spaces.
+# If you have multiple words in the variable, they should all start with a capital letter (except the first).
+# This is called camelCase.
+# myBetterNamedVariable = 42
