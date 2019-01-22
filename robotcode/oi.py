e6c2ae62b6e37b4d95630f0425916a8426eedd15
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2019-01-21 18:52:19 -0600

    create controllers for oi

--- robotcode/oi.py
+++ robotcode/oi.py
@@ -1,2 +1,11 @@
+from wpilib.xboxcontroller import XboxController
+from robotcode import robotmap
+
+driverOne = None
+driverTwo = None
+
 def init():
-  pass
+  global driverOne
+  global driverTwo
+  driverOne = XboxController(robotmap.USB_PORT_DRIVER_ONE)
+  driverTwo = XboxController(robotmap.USB_PORT_DRIVER_TWO)
