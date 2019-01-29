82060b60672b0a2ceb368e0ae3c999c09d26d3a3
Claire Griffin <grif5432@arabcityschools.org>
2019-01-29 13:07:53 -0600

    add encoder

--- src/subsystems/hatch.py
+++ src/subsystems/hatch.py
@@ -1,6 +1,5 @@
 from wpilib.command import Subsystem
 from wpilib.solenoid import Solenoid
-from wpilib.digitalinput import DigitalInput
 from commands.hatch.tilt import TiltHatchCommand
 from rev import CANSparkMax, MotorType
 import robotmap
@@ -10,8 +9,7 @@ class HatchSubsystem(Subsystem):
         # Anything the subsystem has goes here
         super().__init__("HatchSubsystem")
         self.angleMotor = CANSparkMax(robotmap.CAN_REV_HATCH_ANGLE, MotorType.kBrushless)
-        self.maxUpSwitch = DigitalInput(robotmap.DIO_PORT_HATCH_MAX_UP)
-        self.minDownSwitch = DigitalInput(robotmap.DIO_PORT_HATCH_MIN_DOWN)
+        self.angleEncoder = self.angleMotor.getEncoder()
         self.hatchLauncher = Solenoid(robotmap.HATCH_LAUNCHER_PCM_PORT)
         self.hatchLauncher.set(False)
 
@@ -23,12 +21,9 @@ class HatchSubsystem(Subsystem):
     
     def tilt(self, speed):
         # This function lets the mechanism tilt
-        if speed > 0 and self.maxUpSwitch.get():
-            self.angleMotor.set(0)
-        elif speed < 0 and self.minDownSwitch.get():
-            self.angleMotor.set(0)
-        else:
-            self.angleMotor.set(speed)
+        self.angleMotor.set(speed)
+        print("Position: " + self.angleEncoder.getPosition())
+        print("Velocity: " + self.angleEncoder.getVelocity())
 
 
     def releaseHatch(self):
