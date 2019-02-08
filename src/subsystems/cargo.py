7c543d19fdb57045e63253e1c8a9daad3b15f8b1
Claire Griffin <grif5432@arabcityschools.org>
2019-02-08 17:54:18 -0600

    remove limit switch

--- src/subsystems/cargo.py
+++ src/subsystems/cargo.py
@@ -1,6 +1,5 @@
 from wpilib.command import Subsystem
 from ctre.wpi_victorspx import WPI_VictorSPX
-from wpilib.digitalinput import DigitalInput
 import robotmap
 
 
@@ -8,22 +7,13 @@ class CargoSubsystem(Subsystem):
     def __init__(self):
         super().__init__('CargoSubsystem')
         self.bandSpinner = WPI_VictorSPX(robotmap.CAN_ID_VICTOR_BAND_SPINNER)
-        self.motorStopper = DigitalInput(robotmap.DIO_PORT_CARGO_MOTOR_STOPPER)
-        self.canSpin = True
-
+        
 
     def spin(self):
-        if self.canSpin:
-            self.bandSpinner.set(0.5)
-        else:
-            self.stop()
-
-        if self.motorStopper.get():
-            self.canSpin = False
+        self.bandSpinner.set(1)
 
 
     def reset(self):
-        self.canSpin = True
         self.stop()
 
 
