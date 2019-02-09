8ee4b2913be92d45e39a56d76dcb3661251999dc
Claire Griffin <grif5432@arabcityschools.org>
2019-02-08 18:23:06 -0600

    merge everything

--- src/oi.py
+++ src/oi.py
@@ -1,10 +1,14 @@
 from wpilib.xboxcontroller import XboxController
 from wpilib.buttons import JoystickButton
-from commands.hatch.retract import RetractCommand
 from commands.cargo.spin import SpinCommand
 from commands.cargo.reset import ResetCommand
+from commands.hatch.retract import RetractCommand
+from commands.hatch.launchhatch import LaunchHatchCommand
+from commands.lift.togglefront import ToggleFrontCommand
+from commands.lift.togglerear import ToggleRearCommand
 import robotmap
 
+driverOne = None
 driverTwo = None
 
 def init():
@@ -14,9 +18,15 @@ def init():
   driverOne = XboxController(robotmap.USB_PORT_DRIVER_ONE)
   driverTwo = XboxController(robotmap.USB_PORT_DRIVER_TWO)
 
+  toggleLauncher = JoystickButton(driverTwo, robotmap.BUTTON_A)
+  toggleLauncher.whenPressed(LaunchHatchCommand())
+  toggleLauncher.whenReleased(RetractCommand())
+
   toggleFront = JoystickButton(driverTwo, robotmap.BUTTON_RIGHT_BUMPER)
+  toggleFront.whenReleased(ToggleFrontCommand())
 
   toggleRear = JoystickButton(driverTwo, robotmap.BUTTON_LEFT_BUMPER)
+  toggleRear.whenReleased(ToggleRearCommand())    
   
   cargoButton = JoystickButton(driverOne, robotmap.BUTTON_A)
   cargoButton.whileHeld(SpinCommand())
